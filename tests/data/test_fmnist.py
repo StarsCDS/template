7f36ab40b8b8f0fa891cdf13de08546b1ee52955
Jeevananthan24 <mjeevananthan241@gmail.com>
2024-07-05 15:28:39 +0530

    Added test_fmnist.py file

--- tests/data/test_fmnist.py
+++ tests/data/test_fmnist.py
@@ -1,15 +1,32 @@
+import sys
+import os
+import unittest
 import torch
-from src.data.fmnist import FMNIST
+
+# Add the src directory to the Python path
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from data.fmnist import get_data_loaders  # Adjust the import as needed
 
 
-def test_init():
-    dataset = FMNIST()
-    assert isinstance(dataset, torch.utils.data.Dataset)
+class TestFashionMNISTDataLoader(unittest.TestCase):
+    def setUp(self):
+        # Assuming get_data_loaders is a function that returns the train and test DataLoaders
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.train_loader, _ = get_data_loaders(script_dir)
+
+    def test_data_loader(self):
+        for images, labels in self.train_loader:
+            self.assertEqual(
+                images.shape[1:], (28, 28), "Image dimensions are incorrect"
+            )
+            self.assertEqual(
+                labels.shape[0],
+                images.shape[0],
+                "Mismatch in batch size of images and labels",
+            )
+            break  # Only check the first batch for simplicity
 
 
-def test_len():
-    assert True
-
-
-def test_getitem():
-    assert True
+if __name__ == "__main__":
+    unittest.main()
